4f7bcf897602f49e822fa89b46b8e025315c09da
Txmyx <53293648+Toom-boop@users.noreply.github.com>
2021-02-28 17:01:25 +0100

    Add files via upload

---
+++ src/Pointer.java
@@ -0,0 +1,48 @@
+package com.neuralnetwork.advanced;
+
+public class Pointer {
+	
+	int i = 0;
+	int dimension;
+	Size size;
+	
+	Point p;
+	
+	Pointer child;
+	
+	public Pointer(Size size) {
+		this(0, size);
+	}
+	
+	private Pointer(int dimension, Size size) {
+		this.size = size;
+		this.dimension = dimension;
+		this.p = new Point();
+	}
+	
+	public boolean hasNext() {
+		return i<size.get(dimension) && !size.isEnd(p);
+	}
+	
+	public Point next() {
+		if(hasNext()) {
+			p.set(dimension, i);
+			if(dimension<size.getDimensions()-1) {
+				if(child == null) {
+					child = new Pointer(dimension+1, size);
+				}
+				if(child.hasNext()) {
+					p = child.next();
+				}else {
+					child.i = 0;
+					p.set(dimension+1, 0);
+					i++;
+					return next();
+				}
+			}else {
+				i++;
+			}
+		}
+		return p;
+	}
+}
